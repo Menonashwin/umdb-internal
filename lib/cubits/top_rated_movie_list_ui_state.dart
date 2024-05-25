a2e4d4fcf407d5fc1061fbe53965421da704d47a
Ashwin m menon <menonashwin2002@gmail.com>
2024-05-25 23:09:56 +0530

    commit

---
+++ lib/cubits/top_rated_movie_list_ui_state.dart
@@ -0,0 +1,14 @@
+import 'package:umdb/models/top_rated_movie_response.dart';
+
+sealed class TopRatedMovieListUiState {}
+
+class Initial extends TopRatedMovieListUiState {}
+
+class Loading extends TopRatedMovieListUiState {}
+
+class Success extends TopRatedMovieListUiState {
+  Success(this.movieList);
+  final List<TopRatedMovie> movieList;
+}
+
+class Error extends TopRatedMovieListUiState {}
